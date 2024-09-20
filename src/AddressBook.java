2885faaf8b19f10dae01741f4a438631d82280e9
DanielAfanassiev <daniel@javace.com>
2024-09-20 13:33:07 -0400

    First Commmit test

---
+++ src/AddressBook.java
@@ -0,0 +1,14 @@
+public class AddressBook {
+    BuddyInfo buddy1;
+    BuddyInfo buddy2;
+    BuddyInfo buddy3;
+    public void addBuddy(BuddyInfo buddy){
+
+    }
+    public BuddyInfo getBuddy(){
+        return buddy1;
+    }
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+}
