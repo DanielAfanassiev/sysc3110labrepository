eb58c92bdcf78037830b76fe86c1b02574c219e0
DanielAfanassiev <daniel@javace.com>
2024-09-20 14:00:38 -0400

    Third commit test

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,17 +1,18 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBook {
-    List<BuddyInfo> buddyList;
+    ArrayList<BuddyInfo> buddyList;
     public AddressBook(){
+        buddyList = new ArrayList<>();
     }
-    List<BuddyInfo> Add;
 
     public void addBuddy(BuddyInfo buddy){
         buddyList.add(buddy);
     }
-    public BuddyInfo getBuddy(){
-        return buddyList.get(0);
+    public BuddyInfo getBuddy(int index){
+        return buddyList.get(index);
     }
     public void removeBuddy(BuddyInfo buddyToRemove) {
         this.buddyList.remove(buddyToRemove);
@@ -21,6 +22,7 @@ public class AddressBook {
         BuddyInfo buddy = new BuddyInfo("Daniel","Carleton","(123)456-7890");
         AddressBook myAddressBook = new AddressBook();
         myAddressBook.addBuddy(buddy);
+        System.out.println(myAddressBook.getBuddy(0).getName());
         myAddressBook.removeBuddy(buddy);
 
     }
