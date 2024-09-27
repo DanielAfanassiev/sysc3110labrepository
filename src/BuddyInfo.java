f71f0e6b6b95d126a86f20bc3694d7c15636d6c3
DanielAfanassiev <daniel@javace.com>
2024-09-27 13:26:47 -0400

    First git commit with fixed issues. Added BuddyInfo to project

---
+++ src/BuddyInfo.java
@@ -0,0 +1,31 @@
+public class BuddyInfo {
+    public BuddyInfo(){
+        this.name = "Default name";
+        this.address = "Default address";
+        this.phoneNumber = "Default phoneNumber";
+    }
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+    public static void main(String[] args) {
+        BuddyInfo buddy1 = new BuddyInfo("Daniel Afanassiev", "123 Address Drive", "(123) 456-7890");
+        System.out.println("Hello " + buddy1.getName());
+    }
+}
